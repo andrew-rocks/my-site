bd61b53f5094a729ee7b6e05e535daf0f3300172
andrew-rocks <arocks@nd.edu>
2023-07-20 10:47:03 -0500

    Playing around with Gatsby tutorial

---
+++ src/components/layout.tsx
@@ -0,0 +1,41 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { 
+    container,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLinkText
+ } from "./layout.module.css"
+
+interface LayoutProps {
+    pageTitle: string;
+    children: React.ReactNode;
+}
+
+const Layout = ({ pageTitle, children }: LayoutProps) => {
+    return (
+        <div className={container}>
+            <nav>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}>
+                        <Link to="/" className={navLinkText}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/about" className={navLinkText}>
+                            About
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <main>
+                <h1 className={heading}>{pageTitle}</h1>
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default Layout
