7b0bc5ce48dc6948bddfdbb3c2476fd488c4544a
andrew-rocks <arocks@nd.edu>
2023-11-25 11:58:50 -0500

    finished practice blog w/ gatsby images

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -3,11 +3,27 @@ import { graphql } from "gatsby";
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
 import { PageProps } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 const BlogPost = ({ data, children }: PageProps<Queries.BlogPostQuery>) => {
+  const image = getImage(
+    data.mdx?.frontmatter?.hero_image?.childImageSharp ?? null,
+  );
   return (
     <Layout pageTitle={data.mdx?.frontmatter?.title ?? ""}>
-      <p>{data.mdx?.frontmatter?.date ?? ""}</p>
+      <p>Posted: {data.mdx?.frontmatter?.date ?? ""}</p>
+      {image && (
+        <GatsbyImage
+          image={image}
+          alt={data.mdx?.frontmatter?.hero_image_alt ?? ""}
+        />
+      )}
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx?.frontmatter?.hero_image_credit_link ?? ""}>
+          {data.mdx?.frontmatter?.hero_image_credit_text ?? ""}
+        </a>
+      </p>
       {children}
     </Layout>
   );
@@ -19,6 +35,14 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
